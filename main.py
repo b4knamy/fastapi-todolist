e1dc50565341fb1c7f9cf0e8926f2f65c708ff58
Rogério França <rogeriofranca.contato@gmail.com>
2025-01-04 13:32:27 -0300

    SQLAlchemy mudado para SQLModel e configurações necessárias + Endpoints para operações CRUD completas

--- main.py
+++ main.py
@@ -1,34 +1,72 @@
-from typing import Union
-from fastapi import FastAPI
-from database import db_session
-from models.task import Task, TaskSerializer
-from utils import get_paginated_tasks
 
-# random_task_andamento = {
-#     "titulo": "random_title 2",
-#     "descricao": "random description 2",
-#     "estado": "Em andamento"
-# }
-# random_task_concluido = {
-#     "titulo": "random_title 2",
-#     "descricao": "random description 2",
-#     "estado": "Concluído"
-# }
-# random_task_pendente = {
-#     "titulo": "random_title 2",
-#     "descricao": "random description 2",
-#     "estado": "Pendente"
-# }
+from fastapi import FastAPI, HTTPException
+from pydantic import ValidationError
+from database import create_db_and_tables, SessionDep
+from models.task import ALLOWED_STATE_FILTER, Task, TaskCreate, TaskUpdate
+from sqlmodel import select
+from utils import get_paginated_tasks, get_task_or_404
 
 
-app = FastAPI()
+def lifespan(app: FastAPI):
+    create_db_and_tables()
+    yield
 
 
-@app.get("/api/tasks/{page}")
-def read_tasks(page: int, state: Union[str | None]):
-    if not state == "all":
-        tasks = db_session.query(Task).where(Task.estado == state)
+app = FastAPI(lifespan=lifespan)
+
+
+@app.get("/api/tasks/{id}")
+def read_tasks(session: SessionDep, id: int):
+    return get_task_or_404(session=session, task=Task, id=id)
+
+
+@app.get("/api/tasks/list/{page}")
+def list_tasks(session: SessionDep, page: int = 1, estado: str | None = None):
+    if estado:
+        if not estado in ALLOWED_STATE_FILTER:
+            raise HTTPException(status_code=400, detail=f"Possiveis filtros de estado: {
+                                ALLOWED_STATE_FILTER}")
+        query = select(Task).where(Task.estado == estado)
     else:
-        tasks = db_session.query(Task)
-    paginated_tasks = get_paginated_tasks(tasks, page)
-    return paginated_tasks
+        query = select(Task)
+    paginated_query = get_paginated_tasks(page=page, select_query=query)
+    tasks = session.exec(paginated_query).all()
+    return tasks
+
+
+@app.patch("/api/tasks/{id}", response_model=TaskUpdate)
+def update_tasks(id: int, task: TaskUpdate, session: SessionDep):
+    if task.estado and not task.is_state_valid(raise_error=True):
+        pass
+
+    task_data = get_task_or_404(session=session, task=Task, id=id)
+    task_serializer = task.model_dump(exclude_unset=True)
+    task_data.sqlmodel_update(task_serializer)
+
+    session.add(task_data)
+    session.commit()
+    session.refresh(task_data)
+    return task_data
+
+
+@app.post("/api/tasks", response_model=TaskCreate)
+def create_tasks(session: SessionDep, task_create: TaskCreate):
+    try:
+        new_task = Task.model_validate(task_create)
+    except ValidationError:
+        raise HTTPException(
+            status_code=400, detail=task_create.validation_error_message)
+
+    session.add(new_task)
+    session.commit()
+    session.refresh(new_task)
+    return new_task
+
+
+@app.delete("/api/tasks/{id}")
+def delete_tasks(session: SessionDep, id: int):
+    task = get_task_or_404(session=session, task=Task, id=id)
+
+    session.delete(task)
+    session.commit()
+    return {"ok": True}
