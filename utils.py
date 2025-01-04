816b5f3cca58785cbb63d87ea70bc89adec9a91d
Rogério França <rogeriofranca.contato@gmail.com>
2025-01-04 17:17:19 -0300

    foi adicionado funções relacionadas a autenticação JWT de usuário

--- utils.py
+++ utils.py
@@ -1,10 +1,12 @@
 
 from pydantic import BaseModel, ConfigDict
+from typing import Annotated
 from datetime import datetime, timezone
-from settings import PAGINATION_PER_PAGE
-from fastapi import HTTPException
-from database import SessionDep
+from settings import JWT_EXPIRATION_TIME, PAGINATION_PER_PAGE, JWT_SECRET_KEY, JWT_HASH_ALGORITHM
+from fastapi import Depends, HTTPException
+from database import SessionDep, oauth_scheme
 from sqlmodel.sql._expression_select_cls import SelectOfScalar
+from jwt import encode, decode, ExpiredSignatureError, InvalidTokenError
 
 
 class BaseModelSerializer(BaseModel):
@@ -29,3 +31,29 @@ def get_task_or_404(session: SessionDep, task, id: int):
         raise HTTPException(
             status_code=404, detail="Tarefa não encontrada.")
     return task_data
+
+
+def encode_user(payload: dict[str]):
+    to_encode = payload.copy()
+    to_encode["exp"] = get_utc_now() + JWT_EXPIRATION_TIME
+    print(to_encode)
+    print("\n\n")
+    return encode(to_encode, key=JWT_SECRET_KEY,
+                  algorithm=JWT_HASH_ALGORITHM)
+
+
+def decode_user(token: str):
+
+    try:
+        return decode(token, key=JWT_SECRET_KEY,
+                      algorithms=(JWT_HASH_ALGORITHM))
+    except ExpiredSignatureError:
+        raise HTTPException(status_code=401, detail="Token expirado.")
+    except InvalidTokenError:
+        raise HTTPException(status_code=401, detail="Token invalido")
+    except Exception:
+        raise HTTPException(status_code=400, detail="Algo deu errado!")
+
+
+def get_current_user(token: Annotated[str, Depends(oauth_scheme)]):
+    return decode_user(token)
