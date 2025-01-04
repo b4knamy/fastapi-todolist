b8a01b882a386a21569448922dcc231c2489ee10
Rogério França <rogeriofranca.contato@gmail.com>
2025-01-04 13:33:30 -0300

    adicionado função de retorno de model ou Exceção - get_task_or_404

--- utils.py
+++ utils.py
@@ -1,8 +1,10 @@
 
 from pydantic import BaseModel, ConfigDict
 from datetime import datetime, timezone
-from sqlalchemy.orm import Query
 from settings import PAGINATION_PER_PAGE
+from fastapi import HTTPException
+from database import SessionDep
+from sqlmodel.sql._expression_select_cls import SelectOfScalar
 
 
 class BaseModelSerializer(BaseModel):
@@ -13,6 +15,17 @@ def get_utc_now():
     return datetime.now(timezone.utc)
 
 
-def get_paginated_tasks(q: Query, page: int):
+def get_paginated_tasks(page: int, select_query: SelectOfScalar):
+    if not isinstance(page, int) or page < 1:
+        page = 1
+
     initial = page * PAGINATION_PER_PAGE - PAGINATION_PER_PAGE
-    return q.offset(initial).limit(PAGINATION_PER_PAGE).all()
+    return select_query.offset(initial).limit(PAGINATION_PER_PAGE)
+
+
+def get_task_or_404(session: SessionDep, task, id: int):
+    task_data = session.get(task, id)
+    if not task_data:
+        raise HTTPException(
+            status_code=404, detail="Tarefa não encontrada.")
+    return task_data
