e6f88ee63d8524455d4748176d44592cb78ecb48
Rogério França <rogeriofranca.contato@gmail.com>
2025-01-04 00:03:55 -0300

    criação da função de paginação, caso necessário para outras endpoints

--- utils.py
+++ utils.py
@@ -1,5 +1,8 @@
+
 from pydantic import BaseModel, ConfigDict
 from datetime import datetime, timezone
+from sqlalchemy.orm import Query
+from settings import PAGINATION_PER_PAGE
 
 
 class BaseModelSerializer(BaseModel):
@@ -8,3 +11,8 @@ class BaseModelSerializer(BaseModel):
 
 def get_utc_now():
     return datetime.now(timezone.utc)
+
+
+def get_paginated_tasks(q: Query, page: int):
+    initial = page * PAGINATION_PER_PAGE - PAGINATION_PER_PAGE
+    return q.offset(initial).limit(PAGINATION_PER_PAGE).all()
