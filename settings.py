21106acdaf4e93594e4f3b2e78a7eaf2968243ac
Rogério França <rogeriofranca.contato@gmail.com>
2025-01-04 17:18:10 -0300

    adicionado configurações do JWT

--- settings.py
+++ settings.py
@@ -1,1 +1,8 @@
+from secrets import token_hex
+from datetime import timedelta
+
 PAGINATION_PER_PAGE = 5
+
+JWT_SECRET_KEY = token_hex(32)
+JWT_HASH_ALGORITHM = "HS256"
+JWT_EXPIRATION_TIME = timedelta(hours=1)
