e9a367e31fb68b1bdc3f9dda0dabe01c5650e512
Rogério França <rogeriofranca.contato@gmail.com>
2025-01-05 04:19:24 -0300

    foi mudado a url da criação de usuários e adicionado as funcionalidade da hashing de passwords

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 
+from typing import Annotated
 from fastapi import Depends, FastAPI, HTTPException, Request
+from fastapi.security import OAuth2PasswordRequestForm
 from pydantic import ValidationError
 from database import create_db_and_tables, SessionDep
-from models.task import ALLOWED_STATE_FILTER, Task, TaskCreate, TaskUpdate
+from models.task import ALLOWED_STATE_FILTER, Task, TaskCreate, TaskSerializer, TaskUpdate
 from sqlmodel import select
 from models.user import User, UserCreate, TokenJWT, UserSerializer
-from utils import encode_user, get_paginated_tasks, get_task_or_404, get_current_user
+from utils import check_hashed_pwd, encode_user, generate_hashed_pwd, get_paginated_tasks, get_task_or_404, get_current_user
+from contextlib import asynccontextmanager
 
 
-def lifespan(app: FastAPI):
+@asynccontextmanager
+async def lifespan(app: FastAPI):
     create_db_and_tables()
     yield
 
@@ -17,41 +21,44 @@ app = FastAPI(lifespan=lifespan)
 
 
 @app.post("/api/auth/token", response_model=TokenJWT)
-def login_user(session: SessionDep, user: UserCreate):
-    validated_data = user.validate_data()
-
+def login_user(session: SessionDep, form: Annotated[OAuth2PasswordRequestForm, Depends()]):
     user_db = session.exec(select(User).where(
-        User.username == validated_data.username).where(User.password == validated_data.password)).first()
-
+        User.username == form.username)).first()
     if not user_db:
-        raise HTTPException(status_code=400, detail="Credentials invalidos.")
+        raise HTTPException(status_code=400, detail="Credenciais invalidos.")
 
-    token = encode_user(validated_data.model_dump())
+    if not check_hashed_pwd(form.password, user_db.password):
+        raise HTTPException(status_code=400, detail="Credenciais invalidos.")
+
+    token = encode_user(
+        {"username": user_db.username, "password": form.password})
     return TokenJWT(access_token=token, token_type="bearer")
 
 
-@app.post("/api/auth/user")
+@app.post("/api/user/create")
 def create_users(session: SessionDep, user: UserCreate):
     validated_data = user.validate_data()
-
     is_user_already_created = session.exec(select(User).where(
         User.username == user.username)).first()
 
     if is_user_already_created:
         raise HTTPException(status_code=400, detail="Usuário já existe.")
 
-    session.add(validated_data)
+    hashed_password = generate_hashed_pwd(validated_data.password)
+
+    new_user = User(username=validated_data.username, password=hashed_password)
+    session.add(new_user)
     session.commit()
     return {"ok": True}
 
 
 @app.get("/api/tasks/{id}")
-def read_tasks(request: Request, session: SessionDep, id: int, current_user: UserSerializer = Depends(get_current_user)):
+def read_tasks(session: SessionDep, id: int, current_user: UserSerializer = Depends(get_current_user)):
     return get_task_or_404(session=session, task=Task, id=id)
 
 
 @app.get("/api/tasks/list/{page}")
-def list_tasks(session: SessionDep, page: int = 1, estado: str | None = None):
+def list_tasks(session: SessionDep, page: int = 1, estado: str | None = None, current_user: UserSerializer = Depends(get_current_user)):
     if estado:
         if not estado in ALLOWED_STATE_FILTER:
             raise HTTPException(status_code=400, detail=f"Possiveis filtros de estado: {
@@ -64,8 +71,8 @@ def list_tasks(session: SessionDep, page: int = 1, estado: str | None = None):
     return tasks
 
 
-@app.patch("/api/tasks/{id}", response_model=TaskUpdate)
-def update_tasks(id: int, task: TaskUpdate, session: SessionDep):
+@app.patch("/api/tasks/{id}", response_model=TaskSerializer)
+def update_tasks(id: int, task: TaskUpdate, session: SessionDep, current_user: UserSerializer = Depends(get_current_user)):
     if task.estado and not task.is_state_valid(raise_error=True):
         pass
 
@@ -79,8 +86,8 @@ def update_tasks(id: int, task: TaskUpdate, session: SessionDep):
     return task_data
 
 
-@app.post("/api/tasks", response_model=TaskCreate)
-def create_tasks(session: SessionDep, task_create: TaskCreate):
+@app.post("/api/tasks", response_model=TaskSerializer)
+def create_tasks(session: SessionDep, task_create: TaskCreate, current_user: UserSerializer = Depends(get_current_user)):
     try:
         new_task = Task.model_validate(task_create)
     except ValidationError:
@@ -94,7 +101,7 @@ def create_tasks(session: SessionDep, task_create: TaskCreate):
 
 
 @app.delete("/api/tasks/{id}")
-def delete_tasks(session: SessionDep, id: int):
+def delete_tasks(session: SessionDep, id: int, current_user: UserSerializer = Depends(get_current_user)):
     task = get_task_or_404(session=session, task=Task, id=id)
 
     session.delete(task)
