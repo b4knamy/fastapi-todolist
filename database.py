3d05d16b54aa80d25676fd70a9fd9fe0ed2fbc6a
Rogério França <rogeriofranca.contato@gmail.com>
2025-01-05 04:16:56 -0300

    deixando a conexão com o bando de dados dinamico, puxando pelo .env file

--- database.py
+++ database.py
@@ -2,10 +2,21 @@ from fastapi.security import OAuth2PasswordBearer
 from sqlmodel import Session, create_engine, SQLModel
 from typing import Annotated
 from fastapi import Depends
+from dotenv import load_dotenv
+from pathlib import Path
+import os
 
+parent_dir = Path(__file__).resolve().parent
+load_dotenv(parent_dir / ".env")
 
-sqlite_file_name = "sqlite3.db"
-sqlite_url = f"sqlite:///{sqlite_file_name}"
+is_test = os.environ.get("USE_DATABASE_TEST", 0)
+
+if is_test == 1:
+    sqlite_url = os.environ.get(
+        "TEST_DATABASE_URL", "sqlite:///sqlite3_test.db")
+else:
+    sqlite_url = os.environ.get("DATABASE_URL", "sqlite:///sqlite3.db")
+
 
 connect_args = {"check_same_thread": False}
 engine = create_engine(sqlite_url, connect_args=connect_args, echo=True)
